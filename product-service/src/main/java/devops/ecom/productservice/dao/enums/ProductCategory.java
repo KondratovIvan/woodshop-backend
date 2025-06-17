7b25ad6f920b386c11c683662701723ccdcb8912
ivankondratov-hacken <i.kondratov@hacken.io>
2025-06-17 23:22:49 +0300

    charm category implemented

--- product-service/src/main/java/devops/ecom/productservice/dao/enums/ProductCategory.java
+++ product-service/src/main/java/devops/ecom/productservice/dao/enums/ProductCategory.java
@@ -1,5 +1,5 @@
 package devops.ecom.productservice.dao.enums;
 
 public enum ProductCategory {
-    APPLE, FIR, ELM, CYPRESS, POPLAR, CEDAR, PINE, WILLOW, LINDEN, HAZEL, ROWAN, MAPLE, WALNUT, JASMINE
+    CHARM, APPLE, FIR, ELM, CYPRESS, POPLAR, CEDAR, PINE, WILLOW, LINDEN, HAZEL, ROWAN, MAPLE, WALNUT, JASMINE
 }
